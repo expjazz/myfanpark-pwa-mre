e0fc98b90d0a65e9321c6fd275cf8e1d4848eb13
Expedito Andrade <expeditojazz@gmail.com>
2022-08-17 09:45:45 -0300

    initial data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,47 @@
+import axios from 'axios'
+import Script from 'next/script'
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [apiData, setApiData] = useState<string>('')
+  const fetchData = async () => {
+    try {
+      const res = await axios.get('https://app.staging.starsona.com/api/v2/partner/partner_config/', {
+        headers: {
+          device: 'web',
+          'Content-Type': 'application/json',
+        }
+      })
+      if (res && res.data) {
+        setApiData('Api call loaded')
+      }
+    } catch {
+      setApiData("Api call failed")
+    }
+  }
+  useEffect(() => {
+    fetchData()
+  }, [])
   return (
     <div className={styles.container}>
+      <Script
+        type="text/javascript"
+				src="https://appx/web-view.min.js"
+				strategy='lazyOnload'
+				onLoad={() => { alert('script loaded') }}
+        onError={() => { alert('script error') }}
+				// onError={() => {alert('vodapay script error');}}
+      />
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1>
+          A popup will be shown if the script was loaded successfully or if it had an error.
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        <p>
+          {apiData}
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
